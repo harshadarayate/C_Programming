3624b003901bde1569bd9a8d87583d34a21e6418
harshadarayate <hr215144@gmail.com>
2025-10-28 19:51:47 +0530

    Assignment4 Q1 to Q5

---
+++ Assignments/Assignment_4/program4_3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int NonFact(int iNo)
+{
+    int iCnt = 0;
+
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if ((iNo % iCnt) != 0)
+        {
+            printf("%d\t", iCnt);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d", &iValue);
+
+    NonFact(iValue);
+
+    return 0;
+}
