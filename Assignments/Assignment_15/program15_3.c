955f47e9ea68e67e15e5e55871753ef4cdb9a8e6
harshadarayate <hr215144@gmail.com>
2025-11-03 03:54:54 +0530

    Assignment_15 Q1 to Q5

---
+++ Assignments/Assignment_15/program15_3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int CountRange(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if ((iDigit > 3) && (iDigit < 7))
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number  : \n");
+    scanf("%d", &iValue);
+
+    iRet = CountRange(iValue);
+
+    printf("%d\n", iRet);
+
+    return 0;
+}
