896cf003170b478bcb575dee9f7f6aa573106abe
harshadarayate <hr215144@gmail.com>
2025-12-25 14:56:25 +0530

    Assignment_32 Questions

---
+++ Assignments/Assignment_32/program32_3.c
@@ -0,0 +1,44 @@
+
+
+#include <stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int iCnt = 0;
+    int jCnt = 0;
+
+    if (iRow < 0)
+    {
+        iRow = -iRow;
+    }
+    if (iCol < 0)
+    {
+        iCol = -iCol;
+    }
+
+    for (iCnt = 1; iCnt <= iRow; iCnt++)
+    {
+        for (jCnt = iCol; jCnt > 0; jCnt--)
+        {
+            if(iCnt == 1 || jCnt == 1 || iCnt == iRow || jCnt == iCol || iCnt == jCnt)
+            {
+                printf("*\t");
+
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+    return 0;
+}
